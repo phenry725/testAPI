4aaaecc31be8cf8ae1b7ad7b335550e3a4a4ae71
Paul Henry <paulh725@gmail.com>
2018-10-31 20:18:52 -0700

    initial commit of simple api

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+  "encoding/json"
+  "log"
+)
+
+type Response struct {
+	Name string `json:"name"`
+}
+
+func main() {
+
+	//API
+	router := mux.NewRouter()
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "0.0.0.0:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	//routes for leagues
+	router.HandleFunc("/", testMethod).Methods("GET")
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func testMethod(w http.ResponseWriter, r *http.Request) {
+  log.Println("Request")
+	test := Response{Name: "Blah"}
+	json.NewEncoder(w).Encode(test)
+}
